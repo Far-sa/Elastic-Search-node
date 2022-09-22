8d41bfc03b3bfa7487f782528d1c6b62c444f044
Far-sa <fmohtasham82@gmail.com>
2022-09-22 17:41:09 +0300

    Add some config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-
-const app = express()
-const server = require('http').createServer(app)
-const PORT = process.env.PORT
+const dotenv = require('dotenv')
 const expressEjsLayouts = require('express-ejs-layouts')
 
+const app = express()
+
+//* ENV Configurations
+dotenv.config()
+
 //* Configurations
-app.use(express.static())
+app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -15,3 +17,10 @@ app.use(expressEjsLayouts)
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.set('layout', './layouts/master')
+
+//* lunch Server
+const PORT = process.env.PORT || 5000
+const server = require('http').createServer(app)
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`)
+})
